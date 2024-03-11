70560a741630002469fee93b75fc36942abd99f9
JackXLin <jack.llll@yahoo.co.uk>
2024-03-11 18:21:04 +0000

    Third commit, add save_json method and improved the formatting for get_detail method

--- postcodes.py
+++ postcodes.py
@@ -1,10 +1,15 @@
 from postcodes_class import *
 
-postcodes1 = Postcodes("TW13 6JH")
-postcodes1.get_detail()
+# postcodes1 = Postcode("TW13 6JH")
+# postcodes1.get_detail()
+#
+# postcodes2 = Postcode("SW1A 1AA")
+# postcodes2.get_detail()
+# postcodes2.save_json("json_test")
+#
+# postcodes3 = Postcode("TW99 9ZZ")
+# postcodes3.get_detail()
 
-postcodes2 = Postcodes("SW1A 1AA")
-postcodes2.get_detail()
-
-postcodes3 = Postcodes("TW99 9ZZ")
-postcodes3.get_detail()
+postcodes4_6 = Postcodes(["OX49 5NU", "M32 0JG", "NE30 1DP"])
+postcodes4_6.get_detail()
+postcodes4_6.save_json("json_test")
--- postcodes_class.py
+++ postcodes_class.py
@@ -2,18 +2,48 @@ import requests
 import json
 
 
-class Postcodes:
+class Postcode:
     def __init__(self, postcode):
         self.postcode = postcode
+        self.url = f"https://api.postcodes.io/postcodes/{self.postcode}"
+        self.urls = "https://api.postcodes.io/postcodes"
 
     def get_detail(self):
-        url = f"https://api.postcodes.io/postcodes/{self.postcode}"
-        response = requests.get(url)
+        response = requests.get(self.url)
 
         if response.status_code == 200:
             data = response.json()
             print(f"Postcode: {data['result']['postcode']}")
             print(f"Primary Care Trust: {data['result']['primary_care_trust']}")
             print(f"Parliamentary Constituency: {data['result']['parliamentary_constituency']}")
+            return data
         else:
             print(f"Error: {response.status_code}")
+
+    def save_json(self, name):
+        with open(name + ".json", "w") as outfile:
+            json.dump(self.get_detail(), outfile)
+
+
+class Postcodes(Postcode):
+    def __init__(self, postcode):
+        super().__init__(postcode)
+
+    def get_detail(self):
+        data = {
+            "postcodes": [self.postcode]
+        }
+        response = requests.post(self.urls, data=data)
+
+        if response.status_code == 200:
+            result = response.json()
+            for query in result["result"]:
+                for info in query["result"]:
+                    if info in ["postcode", "primary_care_trust", "parliamentary_constituency"]:
+                        print(f"Postcode: {query["result"][info]}")
+                        # print(f"Primary Care Trust: {query["result"][info]}")
+                        # print(f"Parliamentary Constituency: {query["result"][info]}")
+            return result
+        else:
+            print(f"Error: {response.status_code}")
+
